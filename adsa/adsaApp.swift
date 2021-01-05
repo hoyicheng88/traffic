e7d98a8765167edb80553b1207c9d13d52818338
Starmy <mf99250@gmail.com>
2021-01-05 12:43:00 +0800

    Initial Commit

---
+++ adsa/adsaApp.swift
@@ -0,0 +1,17 @@
+//
+//  adsaApp.swift
+//  adsa
+//
+//  Created by User19 on 2021/1/5.
+//
+
+import SwiftUI
+
+@main
+struct adsaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
